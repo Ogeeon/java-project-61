dbfecc0a7a012fd6a73b68beb92335ef80efa531
Alexey <avbogatyrev@gmail.com>
2025-02-23 23:47:04 +1000

    Add implementation of 'Even' game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,40 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    private static final int CHOICE_EXIT = 0;
+    private static final int CHOICE_GREET = 1;
+    private static final int CHOICE_EVEN = 2;
+
     public static void main(String[] args) {
-        Cli.greetUser();
+        String greeting = "Please enter the game number and press Enter.\n"
+                + CHOICE_GREET + " - Greet\n"
+                + CHOICE_EVEN + " - Even\n"
+                + CHOICE_EXIT + " - Exit\n"
+                + "Your choice: ";
+        System.out.println(greeting);
+        int choice = -1;
+        while (choice < CHOICE_EXIT || choice > CHOICE_EVEN) {
+            choice = getChoice();
+            switch (choice) {
+                case CHOICE_EXIT -> System.exit(0);
+                case CHOICE_GREET -> Cli.greetUser();
+                case CHOICE_EVEN -> EvenGame.run();
+                default -> {
+                    System.out.println(greeting);
+                }
+            }
+        }
+
+    }
+
+    private static int getChoice() {
+        Scanner sc = new Scanner(System.in);
+        try {
+            return sc.nextInt();
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,15 +3,16 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greetUser() {
+    public static String greetUser() {
         System.out.println("Welcome to the Brain Games!");
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String playerName = sc.nextLine();
-        while (playerName.isEmpty()) {
+        while (playerName.isBlank()) {
             System.out.print("Sorry, didn't get it. What is your name? ");
             playerName = sc.nextLine();
         }
         System.out.println("Hello, " + playerName + "!");
+        return playerName;
     }
 }
