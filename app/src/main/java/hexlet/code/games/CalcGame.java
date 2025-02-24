b1742787119f3b51c89af3dc0ecb22daab656ea5
Alexey <avbogatyrev@gmail.com>
2025-02-25 09:02:42 +1000

    Switch to using java.util.Random

--- app/src/main/java/hexlet/code/games/CalcGame.java
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -1,11 +1,12 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import java.util.Random;
 
 public class CalcGame {
     private static final int MAX_ROUNDS = 3;
     private static final int MAX_NUMBER = 100;
-    private static final double HALF = 0.5;
+
     public static void startGame() {
         String instructions = "What is the result of the expression?";
         String[] questions = new String[MAX_ROUNDS];
@@ -14,12 +15,13 @@ public class CalcGame {
         int b;
         int result;
         boolean isSum;
+        Random r = new Random();
 
-        for (int i = 0; i < MAX_ROUNDS; i++) {
-            a = (int) (MAX_NUMBER * Math.random());
-            b = (int) (MAX_NUMBER * Math.random());
-            isSum = Math.random() > HALF;
-            questions[i] = "Question: " + a + (isSum ? " + " : " - ") + b;
+        for (int i = 0; i < Engine.MAX_ROUNDS; i++) {
+            a = r.nextInt(MAX_NUMBER);
+            b = r.nextInt(MAX_NUMBER);
+            isSum = r.nextBoolean();
+            questions[i] = String.format("Question: %d %s %d", a, (isSum ? " + " : " - "), b);
             result = a + (isSum ? 1 : -1) * b;
             answers[i] = String.valueOf(result);
         }
