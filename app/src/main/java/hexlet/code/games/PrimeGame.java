ca68ee2660a8ba0697af6a8352465f8e93135739
Alexey <avbogatyrev@gmail.com>
2025-03-08 14:06:34 +1000

    Fix isPrime's definition of prime numbers

--- app/src/main/java/hexlet/code/games/PrimeGame.java
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -24,6 +24,9 @@ public class PrimeGame {
     }
 
     private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
